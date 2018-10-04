83920e8d7631b95909b58f903e24b378f177e2c4
steveyout <34503856+steveyout@users.noreply.github.com>
2018-10-04 22:43:08 +0300

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ import redis
 redisConn = redis.StrictRedis(host='redis', port=6379, db=0)
 
 # Создаем пул соединения с БД
-engine = create_engine('mysql://%s:%s@%s/%s?charset=utf8mb4' % ("root", "litva", "mysql", "bitcoin_bot"), encoding=' utf-8',convert_unicode=True, pool_recycle=1800)
+engine = create_engine('mysql://%s:%s@%s/%s?charset=utf8mb4' % ("uclyyshxzsjli6mk", "LooGA4WJvx5Ld0yKKeP", "bvfolfgqu-mysql.services.clever-cloud.com", "bvfolfgqu"), encoding=' utf-8',convert_unicode=True, pool_recycle=1800)
 db_session = scoped_session(sessionmaker(autocommit=True,
                                          bind=engine))
 Base = declarative_base()
